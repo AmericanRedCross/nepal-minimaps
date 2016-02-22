2f74f054350c6694800ac62b1457266ed58ac2ce
danbjoseph <danbjoseph@gmail.com>
2016-02-22 23:51:33 +0545

    more than just an embedded gist :/

---
+++ nuwakot-visit/js/main.js
@@ -0,0 +1,71 @@
+var map = L.map('map').setView([0, 0], 2);
+
+var info = L.control();
+
+function overFeature(e) {
+    var layer = e.target;
+		info.update(layer.feature.properties);
+}
+function outFeature(e) {
+	info.update();
+}
+function clickFeature(e) {
+    var links = [{
+        title: e.target.feature.properties.place,
+        href: 'img/pics/' + e.target.feature.properties.image,
+        type: 'image/jpeg'
+    }]
+    var options = {
+      toggleControlsOnReturn: false,
+    }
+    blueimp.Gallery(links, options);
+}
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: overFeature,
+        mouseout: outFeature,
+        click: clickFeature
+    });
+}
+
+
+L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+}).addTo(map);
+
+var lineLayer = L.geoJson(trip, {
+  style: {
+      "weight": 5,
+      "opacity": 0.65,
+      "clickable": false
+  }
+}).addTo(map);
+
+var markerLayer = L.geoJson(points, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.MakiMarkers.icon({icon: feature.properties["marker-symbol"], color: feature.properties["marker-color"], size: feature.properties["marker-size"]})
+    });
+  },
+  onEachFeature: onEachFeature
+}).addTo(map);
+
+
+map.fitBounds(lineLayer.getBounds().pad(0.2));
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    infoHtml = '<h5>Nuwakot field visit</h5>';
+    if(props){
+      infoHtml += '<b>' + props.place + '</b><br><small><i>Click for picture<i></small>';
+    } else {
+      infoHtml += 'Hover over a marker<br><small><i> <i></small>';
+    }
+    this._div.innerHTML = infoHtml;
+};
+info.addTo(map);
---
+++ nuwakot-visit/js/points.js
@@ -0,0 +1,178 @@
+var points = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#6c8eff",
+        "marker-size": "m",
+        "marker-symbol": "building",
+        "place": "Famous Farm",
+        "image": "famousFarm.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.16464233398438,
+          27.92127443328113
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "circle",
+        "place": "NRCS Headquarters",
+        "image": "kathmandu.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.29099583625793,
+          27.699279069142186
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#6c8eff",
+        "marker-size": "m",
+        "marker-symbol": "building",
+        "place": "Soaltee Crowne Plaza",
+        "image": "kathmandu.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.29210090637207,
+          27.70071345489316
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "circle",
+        "place": "Thulogaun",
+        "stroke": "#6d6e70",
+        "stroke-width": 3,
+        "stroke-opacity": ".8",
+        "image": "school.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.1714551448822,
+          28.026407586134045
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "circle",
+        "place": "NRCS Nuwakot District Office",
+        "image": "kathmandu.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.14213860034941,
+          27.91399361588727
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Group picture",
+        "image": "overlookGroupPic.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.18378257751465,
+          28.02524268650179
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Suspension bridge",
+        "image": "bridge.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.02379417419434,
+          27.83524323486248
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Mason training",
+        "image": "masonTraining.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.17479181289671,
+          28.025147978660474
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Kids",
+        "image": "kids.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.17822504043579,
+          28.027354649714635
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Switch-back",
+        "image": "road.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.16905725002289,
+          28.02472179234345
+        ]
+      }
+    }
+  ]
+}
